03cc7731a2a682d2c57855ac11e908132babc22a
Sergi Guimerà Roig <sergiguimeraroig@gmail.com>
2024-11-14 19:24:20 +0100

    canviar constructors per error profe

--- BlockChain.py
+++ BlockChain.py
@@ -14,41 +14,54 @@ class rsa_key:
         self.privateExponent = None
         self.modulus = None
         self.primeP, self.primeQ = self.__find_primes()
-        self.privateExponentModulusPhiP = self.privateExponent % self.primeP-1
-        self.privateExponentModulusPhiQ = self.privateExponent % self.primeQ-1
+        self.privateExponentModulusPhiP = self.privateExponent % (self.primeP - 1)
+        self.privateExponentModulusPhiQ = self.privateExponent % (self.primeQ - 1)
         self.inverseQModulusP = self.primeQ**(-1) % self.primeP
 
-        self.n = self.primeP * self.primeQ
+        self.modulus = self.primeP * self.primeQ
 
         def __find_primes(self):
             """
             encuentra dos números primeros de longitud self.__modulus_bits/2
             tq ...
             """
-            pass
+            p, q = 0, 0
+            while False:
+                p = sp.randprime(2^(self.__modulus_bits//2 - 1), 2^(self.__modulus_bits//2));
+                # check p
+                q = sp.randprime(2^(self.__modulus_bits//2 - 1), 2^(self.__modulus_bits//2));
+                # check q 
+            return p, q
 
         def __repr__(self):
             return str(self.__dict__)
+
         def sign(self,message):
             """
             Salida: un entero que es la firma de "message" hecha con la clave RSA usando el TCR
             """
-            pass
+            m1 = pow(message, self.privateExponentModulusPhiP, self.primeP)
+            m2 = pow(message, self.privateExponentModulusPhiQ, self.primeQ)
+            h = (self.inverseQModulusP * (m1 - m2)) % self.primeP
+            return m2 + h * self.primeQ
+
         def sign_slow(self,message):
             """
             Salida: un entero que es la firma de "message" hecha con la clave RSA sin usar el TCR
             """
-            return pow(message, self.privateExponent, self.n)
+            return pow(message, self.privateExponent, self.modulus)
 
 class rsa_public_key:
-    def __init__(self, rsa_key):
+    def __init__(self, publicExponent=1, modulus=1):
         """
         genera la clave pública RSA asociada a la clave RSA "rsa_key"
         """
-        self.publicExponent = rsa_key.publicExponent
-        self.modulus = rsa_key.modulus
+        self.publicExponent = publicExponent
+        self.modulus = modulus
+
     def __repr__(self):
         return str(self.__dict__)
+
     def verify(self, message, signature):
         """
         Salida: el booleano True si "signature" se corresponde con la
@@ -56,13 +69,17 @@ class rsa_public_key:
         pública RSA;
         el booleano False en cualquier otro caso.
         """
-        return signature**self.publicExponent == message % self.modulus
+        # return signature**self.publicExponent == message % self.modulus
+        return pow(signature, self.publicExponent, self.modulus) == message
+
+
 class transaction:
-    def __init__(self, message, RSAkey):
+    def __init__(self, message=0, RSAkey=0):
         """
         genera una transaccion firmando "message" con la clave "RSAkey"
         """
-        self.public_key = rsa_public_key(rsa_key=RSAkey)
+        self.__RSAkey = RSAkey
+        self.public_key = rsa_public_key(publicExponent=RSAkey.publicExponent, modulus=RSAkey.modulus)
         self.message = message
         self.signature = RSAkey.sign(message)
 
@@ -76,6 +93,7 @@ class transaction:
         pública RSA;
         el booleano False en cualquier otro caso.
         """
+        return self.public_key.verify(self.message, self.signature)
 
 class block:
     def __init__(self):
@@ -85,19 +103,19 @@ class block:
         
         self.block_hash = None
         self.previous_block_hash = None
-        self.transaction = transaction(message=, RSAkey=)
+        self.transaction = None # transaction(message=, RSAkey=)
         self.seed = random.randint(0, 2**256 -1)
 
 
 
-    """def __f_hash(self):
+    def __f_hash(self):
         entrada=str(self.previous_block_hash)
         entrada=entrada+str(transaction.public_key.publicExponent)
         entrada=entrada+str(transaction.public_key.modulus)
         entrada=entrada+str(transaction.message)
         entrada=entrada+str(transaction.signature)
         entrada=entrada+str(self.seed)
-        self.block_hsh=int(hashlib.sha256(entrada.encode()).hexdigest(),16)"""
+        self.block_hsh=int(hashlib.sha256(entrada.encode()).hexdigest(),16)
         
     def __repr__(self):
         return str(self.__dict__)
@@ -109,10 +127,14 @@ class block:
         - previous_block_hash=0
         - ser válido
         """
+        self.previous_block_hash = 0
+        self.block_hash = self.__f_hash()
+
     def next_block(self, transaction):
         """
         genera un bloque válido seguiente al actual con la transacción "transaction"
         """
+        block(transaction, previous_block_hash=self.block_hash)
 
     def verify_block(self):
         """
@@ -123,9 +145,20 @@ class block:
         Salida: el booleano True si todas las comprobaciones son correctas;
         el booleano False en cualquier otro caso.
         """
+        return self.__verify_previous_hash() and self.__verify_transaction() and self.__verify_previous_hash()
+    
+
+    def __verify_previous_hash(self):
+        return False
+    
+    def __verify_transaction(self):
+        return False
+    
+    def __verify_previous_hash(self):
+        return False
 
 class block_chain:
-    def __init__(self,transaction):
+    def __init__(self, transaction=0):
         """
         genera una cadena de bloques que es una lista de bloques,
         el primer bloque es un bloque "genesis" generado amb la transacción "transaction"
@@ -152,6 +185,25 @@ class block_chain:
 
 """
 TODO:
+    -Prev: mirar Sagemath Aritmética y primalidad y RSA
     -Passar expresions (-1, **,...) a versió correcta (pow(), ...)
+    -Acabar RSA
+    -Debug RSA
+    -RW
+    -Pseudo
+"""
+
+"""
+Ron was wrong, Whit was right
+RESUM:
+    -weak keys
     -
+
+"""
+
+"""
+Pseudo
+RESUM:
+    -
+
 """
